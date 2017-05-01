34784972492de078afcf55e664c945726846a21e
Pieter Ennes <pieter@authentiq.com>
2017-05-01 16:13:20 +0100

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Upload the current directory to the CDN.
+#
+set -e
+
+if [ "$1" = "prod" ]; then
+    bucket="s3://authentiq-cdn/"
+elif [ "$1" = "test" ]; then
+    bucket="s3://authentiq-cdn-test/"
+else
+    echo "Please specify 'prod' or 'test'."
+    exit 1
+fi
+
+aws s3 cp --recursive --acl public-read --exclude ".git/*" --exclude "deploy.sh" --dryrun . $bucket
+
